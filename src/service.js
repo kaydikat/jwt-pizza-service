0c976f9cc6e6dfbd6f23bf4cbd2575fe7e23a394
Kaydance Turner <kaydancegillam@gmail.com>
2025-12-04 11:21:52 -0700

    Create whitelist for who can access my api

--- src/service.js
+++ src/service.js
@@ -14,7 +14,15 @@ app.use(express.json());
 app.use(logger.httpLogger);
 app.use(setAuthUser);
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", req.headers.origin || "*");
+  const allowedOrigins = [
+    'https://pizza.kaydanceturner.click', 
+    'http://localhost:3000'
+  ];
+
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.setHeader("Access-Control-Allow-Credentials", "true");
